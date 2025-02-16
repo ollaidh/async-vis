0e0cd3d741ccd12a601ffdc8da53d0e6a64d8cd6
ollaidh <ollaidh@programmer.net>
2025-02-16 15:17:08 +0200

    Share session between different requests

--- send_reqs.py
+++ send_reqs.py
@@ -12,10 +12,10 @@ def generate_url(root_url: str) -> str:
 
 
 async def main():
-    for i in range(10):
-        url = generate_url("http://127.0.0.1:8888/stocks")
-        print(i, url)
-        async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession() as session:
+        for i in range(10):
+            url = generate_url("http://127.0.0.1:8888/stocks")
+            print(i, url)
             async with session.get(url) as response:
                 print(response.status)
 
