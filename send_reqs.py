2551ee7021245bf9a71548bd92f9ac8d51bdf5b2
ollaidh <ollaidh@programmer.net>
2025-02-16 15:14:08 +0200

    Make sending requests async

--- send_reqs.py
+++ send_reqs.py
@@ -1,5 +1,6 @@
-import requests
+import aiohttp
 import random
+import asyncio
 
 
 ENDPOINTS = ["history", "info", "sectors"]
@@ -10,9 +11,14 @@ def generate_url(root_url: str) -> str:
     return f"{root_url}/{endpoint}"
 
 
-if __name__ == "__main__":
+async def main():
     for i in range(10):
         url = generate_url("http://127.0.0.1:8888/stocks")
         print(i, url)
-        response = requests.get(url)
-        print(response.status_code)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                print(response.status)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
