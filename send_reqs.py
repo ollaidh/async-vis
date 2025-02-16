676d793b0bcf2f8899dcb933a39ecace7fe4b739
ollaidh <ollaidh@programmer.net>
2025-02-16 15:28:35 +0200

    Send reqs TRULY asynchronously

--- send_reqs.py
+++ send_reqs.py
@@ -11,13 +11,26 @@ def generate_url(root_url: str) -> str:
     return f"{root_url}/{endpoint}"
 
 
-async def main():
+async def send_requests(count: int) -> None:
     async with aiohttp.ClientSession() as session:
-        for i in range(10):
+        for i in range(count):
             url = generate_url("http://127.0.0.1:8888/stocks")
-            print(i, url)
             async with session.get(url) as response:
-                print(response.status)
+                current_task = asyncio.current_task()
+                assert current_task
+                print(
+                    current_task.get_name(),
+                    i,
+                    url,
+                    response.status,
+                )
+
+
+async def main() -> None:
+    tasks = [
+        asyncio.create_task(send_requests(10), name=f"request-{i}") for i in range(4)
+    ]
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
